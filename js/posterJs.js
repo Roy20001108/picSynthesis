6aee83cd06e42805f31eb2a80266bbf405138953
heshuo <hehsuo@boe.com.cn>
2019-01-17 16:35:42 +0800

    首页

--- js/posterJs.js
+++ js/posterJs.js
@@ -1,8 +1,14 @@
 (function(global){
-  $('#tab span').click(function() {
+  $('#operaTab span').click(function() {
     var i = $(this).index();//下标第一种写法
     //var i = $('tit').index(this);//下标第二种写法
     $(this).addClass('selectTab').siblings().removeClass('selectTab');
-    $('#con li').eq(i).show().siblings().hide();
+    $('#templateCon li').eq(i).show().siblings().hide();
+  });
+  $('#templateCon img').click(function() {
+    var i = $(this).index();//下标第一种写法
+    //var i = $('tit').index(this);//下标第二种写法
+    $(this).addClass('selectImg').siblings().removeClass('selectImg');
+    $('#ImgCon li').eq(i).show().siblings().hide();
   });
 })(window);
