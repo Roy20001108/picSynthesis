9d212087606aed77dedcdc1fdb37468dcbd1fd7b
heshuo <hehsuo@boe.com.cn>
2019-01-26 23:06:19 +0800

    修改

--- js/posterJs.js
+++ js/posterJs.js
@@ -12,12 +12,12 @@
   var rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
   var oFReader = new FileReader();
   // var num = 0;
-  alert("进度条加载");
+  // alert("进度条加载");
   imgArry.each(function(i){//遍历获取到的所有img
     var oImg = new Image();//new一个新对象
-    // oImg.onload = function(){//使用onload方法，在加载完成后执行
+    oImg.onload = function(){//使用onload方法，在加载完成后执行
       // document.getElementById('fill').style.width=0;
-      //   oImg.onload = null;//首先清除掉缓存
+        oImg.onload = null;//首先清除掉缓存
       //   num++;//每次加载的过程中num++，即执行次数
       //   // alert(num/$("img").size()*100);
       //   processBar = document.getElementById('fill'),
@@ -35,7 +35,7 @@
       //     $(".loading").fadeOut();//当num的值大于等于个数时隐藏
       //     document.getElementById('makePhoster').style.display='block';
       //   }, 5000);
-      //   }
+        }
         oImg.src = imgArry[i].src;//预加载，先指定一个img.src，当onload成功以后可以将数据指定到某一个元素或者图片上，或者返回一个结果
     })
   var progressbar={
