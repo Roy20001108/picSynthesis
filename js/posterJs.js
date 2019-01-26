3713c91d451ce078507da0ea6d116600771b48b2
heshuo <hehsuo@boe.com.cn>
2019-01-26 10:08:44 +0800

    修改

--- js/posterJs.js
+++ js/posterJs.js
@@ -303,9 +303,9 @@
         } else if (selectImgFlag == 1) {
           var toNameVal = $("input[name='toNameTwo']").val();
           var fromNameVal = $("input[name='fromNameTwo']").val();
-          drawToName(context,toNameVal,88,180,'#ffcc66');
-          var rownum = drawText(context,blessValStr,88,206,438,'#ffcc66');
-          drawFromName(context,fromNameVal,350,(206 + rownum * 50 + 60),'#ffcc66');
+          drawToName(context,toNameVal,88,170,'#ffcc66');
+          var rownum = drawText(context,blessValStr,88,190,430,'#ffcc66');
+          drawFromName(context,fromNameVal,400,(206 + rownum * 50 + 30),'#ffcc66');
         } else if (selectImgFlag == 2) {
           var toNameVal = $("input[name='toNameThree']").val();
           var fromNameVal = $("input[name='fromNameThree']").val();
