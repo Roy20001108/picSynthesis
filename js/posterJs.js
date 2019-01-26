db83b9dc23539df3fa431590e7e4fbeb87976967
heshuo <hehsuo@boe.com.cn>
2019-01-26 21:27:56 +0800

    修改

--- js/posterJs.js
+++ js/posterJs.js
@@ -14,22 +14,21 @@
   var num = 0;
   imgArry.each(function(i){//遍历获取到的所有img
     var oImg = new Image();//new一个新对象
-    // oImg.onload = function(){//使用onload方法，在加载完成后执行
-        // oImg.onload = null;//首先清除掉缓存
-        // num++;//每次加载的过程中num++，即执行次数
-        // // alert(num/$("img").size()*100);
-        // processBar = document.getElementById('fill'),
-        // widthPercentage = Math.round(num/$("img").size()*100);
-        // processBar.style.width = widthPercentage + '%';
-        // $("#percentVal").html("心意值正在加载" + parseInt(num/$("img").size()*100)+"%");
-        // // $(".loading b").html(parseInt(num/$("img").size()*100)+"%");//改变b标签的内容，用num除以img的个数，再乘以100，再取整，这就是加载的百分数
-        //     if(num >= i){
-        //       processBar.style.width = 100 + '%';
-        //       $(".loading").fadeOut();//当num的值大于等于个数时隐藏
-        //       document.getElementById('makePhoster').style.display='block';
-        //     }
-            
-        // }
+    oImg.onload = function(){//使用onload方法，在加载完成后执行
+        oImg.onload = null;//首先清除掉缓存
+        num++;//每次加载的过程中num++，即执行次数
+        // alert(num/$("img").size()*100);
+        processBar = document.getElementById('fill'),
+        widthPercentage = Math.round(num/$("img").size()*100);
+        processBar.style.width = widthPercentage + '%';
+        $("#percentVal").html("心意值正在加载" + parseInt(num/$("img").size()*100)+"%");
+        // $(".loading b").html(parseInt(num/$("img").size()*100)+"%");//改变b标签的内容，用num除以img的个数，再乘以100，再取整，这就是加载的百分数
+            if(num >= i){
+              processBar.style.width = 100 + '%';
+              $(".loading").fadeOut();//当num的值大于等于个数时隐藏
+              document.getElementById('makePhoster').style.display='block';
+            }
+        }
         oImg.src = imgArry[i].src;//预加载，先指定一个img.src，当onload成功以后可以将数据指定到某一个元素或者图片上，或者返回一个结果
     })
   creatCanvas(bgdata1);
@@ -393,13 +392,13 @@
         console.log('imgDatadahe：'+imgDatadahe);
     }
     function drawToName(context,t,x,y,c){
-      context.font='bold 27px Kaiti';
+      context.font='bold 27px KaitiMobile';
       context.textAlign='left';
       context.fillStyle= c;
       context.fillText(t,x,y);
     }
     function drawFromName(context,t,x,y,c){
-      context.font='bold 27px Kaiti';
+      context.font='bold 27px KaitiMobile';
       context.textAlign='right';
       context.fillStyle=c;
       context.fillText(t,x,y);
@@ -410,7 +409,7 @@
       var temp = "";              
       var row = [];
   
-      context.font='bold 27px Kaiti';
+      context.font='bold 27px KaitiMobile';
       context.textAlign='left';
       context.textBaseline='middle';
       context.fillStyle=c;
