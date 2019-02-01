1fcc4e3213986dcffdf3909e296667d87750aaf6
heshuo <hehsuo@boe.com.cn>
2019-02-01 19:13:53 +0800

    修改

--- js/posterJs.js
+++ js/posterJs.js
@@ -379,7 +379,7 @@ progressbar.init();
           var toNameVal = $("input[name='toNameThree']").val();
           var fromNameVal = $("input[name='fromNameThree']").val();
           var rownum;
-          drawToName(context,toNameVal,70,310,'#ffffff');
+          drawToName(context,toNameVal,100,310,'#ffffff');
           if (customFlag) {
             var customWidth = document.getElementsByClassName("textArea")[2].style.width;
             if (customWidth == '100%') {
@@ -388,9 +388,9 @@ progressbar.init();
               customWidth = parseFloat(customWidth);
             }
             var blessValCustom =document.getElementById("textAreaThree").innerText;
-            rownum = drawText(context,blessValCustom,70,330,customWidth,'#ffffff');
+            rownum = drawText(context,blessValCustom,100,330,customWidth,'#ffffff');
           } else {
-            rownum = drawText(context,blessValStr,70,330,440,'#ffffff');
+            rownum = drawText(context,blessValStr,100,330,440,'#ffffff');
           }
           drawFromName(context,fromNameVal,515,(330 + rownum * 50 + 50),'#ffffff');
         } else {
